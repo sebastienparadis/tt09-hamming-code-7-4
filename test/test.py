3590493abd855732f7e25f349dfdd198e28f6e75
Sebastien Paradis <s3paradi@uwaterloo.ca>
2024-10-29 20:10:42 -0400

    Writing mock cocotb test case

--- test/test.py
+++ test/test.py
@@ -26,15 +26,28 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
+    # Test Encoding (mode = 0, 4-bit data = 1010)
+    dut._log.info("Testing Encoding Mode with Data Input: 1010")
+    dut.ui_in.value = 0b00001010  # 4-bit data with MSB (ui_in[7]) = 0 for encoding mode
+    await ClockCycles(dut.clk, 1)
+    # Check expected encoded output
+    # Expected encoded result: parity + data = 1011010
+    expected_encoded = 0b1011010
+    assert dut.uo_out.value == expected_encoded, f"Encoding failed, got {dut.uo_out.value}, expected {expected_encoded}"
+
+    # Test Decoding (mode = 0, 4-bit code = 00001111)
+    dut._log.info("Testing Encoding Mode with Data Input: 1111")
+    dut.ui_in.value = 0b00001111  # 7-bit code with MSB (ui_in[7]) = 1 for decoding mode
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
+    # Expected corrected code: 1101010, decoded data: 1010
+    expected_encoded = 0b1111111
+    assert dut.uo_out.value == expected_encoded, f"Encoding failed, got {dut.uo_out.value}, expected {expected_encoded}"
+
+    dut._log.info("Hamming Code Test completed successfully")
+
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
